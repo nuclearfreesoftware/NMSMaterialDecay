a2d3112166cd694b52a22c1d2db434aa20e93ba5
Moritz Kuett <moritz.kuett@web.de>
2014-05-27 21:53:41 -0400

    file_exists method added

--- include/NMSAlphaNSet.hh
+++ include/NMSAlphaNSet.hh
@@ -4,6 +4,9 @@
 #include <vector>
 #include <fstream>
 #include <iomanip>
+#include <sys/stat.h>
+
+#include "G4SystemOfUnits.hh"
 
 #include "NMSAlphaNReaction.hh"
 
@@ -14,7 +17,13 @@ public:
 
   void saveToFile(G4String filename);
   void loadFromFile(G4String filename);
-  
+
+private:  
+  inline bool file_exists(G4String filename) {
+    struct stat buffer;
+    return (stat (filename.c_str(), &buffer) == 0);
+  }
+
 };
 
 #endif
